24a2512e92d1f03df78ed0f85a1d66c978756370
cybergirl00 <92691928+cybergirl00@users.noreply.github.com>
2024-07-27 12:22:17 +0100

    first commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -41,7 +41,7 @@ const Hero = () => {
             Experience the Taste of Tradition: Authentic Masa Cuisine. Your tommy will thank you
             </p>
             <div className="items-center justify-center gap-x-3 space-y-3 sm:flex sm:space-y-0 xl:justify-start">
-                <a href="javascript:void(0)" className="flex items-center justify-center gap-x-2 py-2 px-4 text-white font-medium bg-primary duration-150 hover:bg-green-300 active:bg-primary rounded-lg md:inline-flex">
+                <a href="#dishes" className="flex items-center justify-center gap-x-2 py-2 px-4 text-white font-medium bg-primary duration-150 hover:bg-green-300 active:bg-primary rounded-lg md:inline-flex">
                   Explore Menu
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                         <path fillRule="evenodd" d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z" clipRule="evenodd" />
